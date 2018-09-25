253cbbf08164edbd912523ba7652713440aa0183
pin84 <qp1984@live.com>
2018-09-25 21:53:32 +0800

    'deletewenquan'

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,30 @@
 <template>
 <div class="wrapper">
-  <input type="file"  name="file" class="inputfile">
-  <label for="file" class="btn">选择文件</label>
+  <input type="file"  name="file" id="inputfile">
+  <label for="inputfile" class="lab" ref="lable">
+    {{msg}}
+  </label>
 </div>
 </template>
 <script>
 export default {
+  data(){
+    return{
+      msg:'选择文件'
+    }
+  },
+  mounted(){
+    this.test()
+  },
+
+  methods:{
+    test(){
+      let lab = this.$refs.lable
+      lab.addEventListener('click',() => {
+        console.log('aaa')
+      })
+    }
+  }
 }
 </script>
 <style lang='stylus' scoped>
@@ -13,7 +32,18 @@ export default {
   // display flex
   border 1px solid red
   position relative
-  .inputfile
+  #inputfile
     position absolute
     clip:rect(0,0,0,0);
+  .lab
+    padding 5px 10px
+    background #336699  
+    box-sizing border-box
+    border-radius 5px 
+    color #fff
+    font-size 14px
+    &:hover
+      cursor pointer
+      background #336666
+
 </style>
